85383cbac955541a29ce4746a03620c6a6d6432a
Maxwell <maxpasqualis@gmail.com>
2022-12-13 16:51:56 -0800

    render one chat entry

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
+
+// temporary for debugging
+const testMessage = chatMessages[0];
 
 const App = () => {
   return (
@@ -11,6 +15,11 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={testMessage.sender}
+          body={testMessage.body}
+          timeStamp={testMessage.timeStamp}
+        ></ChatEntry>
       </main>
     </div>
   );
