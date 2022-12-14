5e55c8c4e3db5699ff5c5fd60c668a214eb1021f
masinakh <nakhjirimasi@gmail.com>
2022-12-13 18:34:29 -0800

    wave 1 is almost completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './App.css';
+
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const chatMessagesData = chatMessages.map((chatMessage) => {
+    return (
+      <li>
+        <ChatEntry
+          sender={chatMessage.sender}
+          body={chatMessage.body}
+          timeStamp={chatMessage.timeStamp}
+        />
+      </li>
+    );
+  });
+
   return (
     <div id="App">
       <header>
@@ -11,6 +25,7 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        {chatMessagesData}
       </main>
     </div>
   );
