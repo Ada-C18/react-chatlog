6bd43da8549d1567bc9d2f16471d109c219f1e44
kimiahasani <kn.improve@gmail.com>
2022-12-13 18:47:37 -0800

    update App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog';
 import chatMessages from './data/messages.json';
 
 const App = () => {
@@ -11,6 +13,12 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry 
+        body = {chatMessages[0].body}
+        sender = {chatMessages[0].sender}
+        timeStamp = {chatMessages[0].timeStamp}
+          />
+        <ChatLog/>
       </main>
     </div>
   );
