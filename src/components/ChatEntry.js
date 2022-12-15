446adc771ed0a8741b4f27b8f25de8fa8ca2ea79
abby <abbychoi97@gmail.com>
2022-12-14 23:44:38 -0800

    completed wave 1, passed tests

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
+  // firstSender or not firstSender
+  const messageSide =
+    props.firstSender === props.sender
+      ? 'chat-entry local'
+      : 'chat-entry remote';
+
   return (
-    <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+    <div className={messageSide}>
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{props.body}</p>
+        <p className="entry-time">
+          <TimeStamp time={props.timeStamp}></TimeStamp>
+        </p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -17,6 +26,10 @@ const ChatEntry = (props) => {
 
 ChatEntry.propTypes = {
   //Fill with correct proptypes
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
+  firstSender: PropTypes.string,
 };
 
 export default ChatEntry;
