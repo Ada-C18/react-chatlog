fd0c76ca4b62e865f1aba594ba8287e8c883fa01
LP <lp.passarelli.wilson@gmail.com>
2022-12-15 13:58:05 -0800

    got the first chat to show up.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
+
+//const chats = chatMessages;
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>🤍 Chat Log 🤍</h1>
       </header>
       <main>
+        <ChatEntry chatMessages={chatMessages} />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -3,12 +3,30 @@ import './ChatEntry.css';
 import PropTypes from 'prop-types';
 
 const ChatEntry = (props) => {
+  // const chatMessages = props.chatMessages;
+  // const chatComponents = [];
+
+  // for (const chat of chatMessages) {
+  //   chatComponents.push(
+  //     <ChatEntry
+  //       key={chat.id}
+  //       id={chat.id}
+  //       sender={chat.sender}
+  //       body={chat.body}
+  //       timestamp={chat.timestamp}
+  //       liked={chat.liked}
+  //     />
+  //   );
+  // }
+
+  const firstChat = props.chatMessages[0];
+
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{firstChat.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{firstChat.body}</p>
+        <p className="entry-time">{firstChat.timestamp}</p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -16,7 +34,11 @@ const ChatEntry = (props) => {
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  id: PropTypes.number.isRequired,
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timestamp: PropTypes.instanceOf(Date).isRequired,
+  liked: PropTypes.bool.isRequired,
 };
 
 export default ChatEntry;
