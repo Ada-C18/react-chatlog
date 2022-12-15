5163a2de86c85090266f062de32a3b3576323a01
Steffanie Lopez <lopezsteffanie8@gmail.com>
2022-12-15 10:55:46 -0800

    working thorugh wave 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import './App.css';
-import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
+// import chatMessages from './data/messages.json';
+const chatMessages = [
+  {
+    id: 1,
+    sender: 'Vladimir',
+    body: 'why are you arguing with me',
+    timeStamp: '2018-05-29T22:49:06+00:00',
+    liked: false
+  }
+]
+
 
 const App = () => {
   return (
@@ -11,6 +22,7 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry chatMessages={chatMessages}/>
       </main>
     </div>
   );
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -3,12 +3,21 @@ import './ChatEntry.css';
 import PropTypes from 'prop-types';
 
 const ChatEntry = (props) => {
+  const messages = props.chatMessages.map((message, i) => {
+    return <chatMessages
+    key={i}
+    sender={message.sender}
+    body={message.body}
+    timeStamp={message.timeStamp}
+    liked={message.liked}
+    />
+  })
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">name: {props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>message: {props.body}</p>
+        <p className="entry-time">time: {props.timeStamp}</p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -16,7 +25,10 @@ const ChatEntry = (props) => {
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp:PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
 };
 
 export default ChatEntry;
