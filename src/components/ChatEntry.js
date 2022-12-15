9ca56ca419f8260fd0f74c4b7f7246936442906a
LP <lp.passarelli.wilson@gmail.com>
2022-12-15 14:05:30 -0800

    got time stamp showing, so now one chat is showing. wave 1 passed

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -3,22 +3,6 @@ import './ChatEntry.css';
 import PropTypes from 'prop-types';
 
 const ChatEntry = (props) => {
-  // const chatMessages = props.chatMessages;
-  // const chatComponents = [];
-
-  // for (const chat of chatMessages) {
-  //   chatComponents.push(
-  //     <ChatEntry
-  //       key={chat.id}
-  //       id={chat.id}
-  //       sender={chat.sender}
-  //       body={chat.body}
-  //       timestamp={chat.timestamp}
-  //       liked={chat.liked}
-  //     />
-  //   );
-  // }
-
   const firstChat = props.chatMessages[0];
 
   return (
@@ -26,7 +10,7 @@ const ChatEntry = (props) => {
       <h2 className="entry-name">{firstChat.sender}</h2>
       <section className="entry-bubble">
         <p>{firstChat.body}</p>
-        <p className="entry-time">{firstChat.timestamp}</p>
+        <p className="entry-time">{firstChat.timeStamp}</p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -37,7 +21,7 @@ ChatEntry.propTypes = {
   id: PropTypes.number.isRequired,
   sender: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
-  timestamp: PropTypes.instanceOf(Date).isRequired,
+  timeStamp: PropTypes.instanceOf(Date).isRequired,
   liked: PropTypes.bool.isRequired,
 };
 
