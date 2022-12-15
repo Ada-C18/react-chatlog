4f6eb5a18afd2f2c51b35e1b9c37456c2f24add5
hang tran <hangmtran101@gmail.com>
2022-12-15 06:34:55 -0800

    chatentry one entry created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
+// import ChatLog from './components/ChatEntry';
 
 const App = () => {
+//
+//       // 'id': 1,
+//       'sender':'Vladimir',
+//       'body':'why are you arguing with me',
+//       'timeStamp':'2018-05-29T22:49:06+00:00',
+//       // 'liked': false
+//       
+  const chatEntry = chatMessages[0]
+  
+
+  
+  
+  render() ;
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry sender= {chatEntry.sender} //passing data from ChatEntry
+                   body= {chatEntry.body}
+                   timeStamp = {chatEntry.timeStamp}>
+
+        </ChatEntry>
+{/* 
+        {/* <ChatLog> 
+          
+        </ChatLog>  */}
+        
+        
+        {/* /* Wave 01: Render one ChatEntry component
+        
+      
+
+        Wave 02: Render ChatLog component */ }
+        
       </main>
     </div>
   );
-};
+      }
+ 
+
 
 export default App;
