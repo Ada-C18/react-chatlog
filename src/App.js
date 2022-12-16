5c1ef2175e85fdd8c86a01dfa75660f09a16272b
Aviva <avivajoy.gars@gmail.com>
2022-12-16 15:44:10 -0700

    merge conflict resolved and added chat message data into App

--- src/App.js
+++ src/App.js
@@ -21,14 +21,14 @@ const chatMessagesData = chatMessages.map((message, i) => {
 
 
 const App = () => {
-  const chatEntry = <ChatEntry />
+  // const chatEntry = <ChatEntry />
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/<ChatEntry />}
+        {chatMessagesData}
       </main>
     </div>
   );
