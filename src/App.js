08bc024bf3b9c2110dfae13203f4da2d9fbbe1d3
Kallie Gannon <gannonkallie@gmail.com>
2022-12-16 17:26:04 -0500

    message-timestamp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import './App.css';
-import chatMessages from './data/messages.json';
 import ChatEntry from './components/ChatEntry';
+import chatMessages from './data/messages.json';
+
+
+
+const chatMessagesData = chatMessages.map((message, i) => {
+
+  return ( 
+    <ChatEntry
+      key={i}
+      id={message.id}
+      sender={message.sender}
+      body={message.body}
+      timeStamp={message.timeStamp}
+      liked={message.liked}
+    />
+  )
+});
+
 
 const App = () => {
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
-        <ChatEntry></ChatEntry>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
-      </main>
+      {chatMessagesData}
+      </main> 
     </div>
   );
 };
