ea28cc7cca5ef67edb7fce5da4fbacba1f999d6c
Kallie Gannon <gannonkallie@gmail.com>
2022-12-16 17:39:05 -0500

    round 2 electric boogaloo

--- src/App.js
+++ src/App.js
@@ -21,15 +21,14 @@ const chatMessagesData = chatMessages.map((message, i) => {
 
 
 const App = () => {
-  const chatEntry = <ChatEntry></ChatEntry>
+  const chatEntry = <ChatEntry />
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        {/<ChatEntry />}
       </main>
     </div>
   );
