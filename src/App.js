bbfba0c9687f7db59daedcae933e880f5e0ba0e4
lisafeng <lisafenglisa@yahoo.com>
2022-12-16 14:07:41 -0500

    getting the first chat content, but not passing the tests, still need working on it

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
+
 const App = () => {
+  const message = chatMessages[0]
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry chat={message}></ChatEntry>
       </main>
     </div>
   );
