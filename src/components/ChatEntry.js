a8802010b169140880190f11cd007e954468e670
Sierra <sierra.r.jordan@gmail.com>
2022-12-15 17:58:48 -0800

    fixes time stamp on messages

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
   const sender = props.sender;
   const body = props.body;
-  const timeStamp = props.timeStamp;
+  const timeStamp = { time: props.timeStamp };
+  const time = TimeStamp(timeStamp);
 
   return (
     <div className="chat-entry local">
       <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
         <p>{body}</p>
-        <p className="entry-time">{timeStamp}</p>
+        <p className="entry-time">{time}</p>
         <button className="like">🤍</button>
       </section>
     </div>
