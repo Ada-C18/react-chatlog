a9164fa633b01b201f4dcf27ef64f461dc61a0f4
Samantha Hunt <ariaandsam@gmail.com>
2022-12-16 12:42:09 -0800

    Finished wave 1, finally

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  console.log(chatMessages[0].sender)
   return (
     <div id="App">
       <header>
@@ -10,7 +12,8 @@ const App = () => {
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        Wave 02: Render ChatLog component */
+        <ChatEntry sender={chatMessages[0].sender} body={chatMessages[0].body} timeStamp={chatMessages[0].timeStamp}/>}
       </main>
     </div>
   );
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -2,21 +2,39 @@ import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 
-const ChatEntry = (props) => {
-  return (
-    <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+const ChatEntry = ({sender, body, timeStamp}) => {
+  const chatComponents = []
+  const newDate = new Date()
+  console.log(new Date(timeStamp))
+  let diff = (newDate.getTime() - new Date(timeStamp).getTime()) / 1000;
+  diff /= (60*60*24);
+  let currentYears = Math.abs(Math.round(diff/365.25))
+  
+  chatComponents.push(
+    <li>
+      <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{body}</p>
+        <p className="entry-time">{currentYears} years ago</p>
         <button className="like">🤍</button>
       </section>
+    </li>
+    )
+  return (
+    <div className="chat-entry local">
+      {chatComponents}
     </div>
   );
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  chatEntry: PropTypes.arrayOf(
+    PropTypes.shape({
+      sender: PropTypes.string.isRequired,
+      body: PropTypes.string.isRequired,
+      timeStamp: PropTypes.string.isRequired
+    })
+  )
 };
 
 export default ChatEntry;
