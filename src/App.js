a2c76dd093a36a8e21c6b6a415662cdeb46d728e
Carmen Vega <cvega23@horizon.csueastbay.edu>
2022-12-16 12:15:59 -0800

    Changed header, added ChatEntry with specific hardcoded info for sender, body, and timestamp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
+// App.js
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Vladimir and Estragon messages</h1>
       </header>
       <main>
+        <ChatEntry
+          sender="Vladimir"
+          body="why are you arguing with me"
+          timeStamp="2018-05-29T22:49:06+00:00"
+        />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
