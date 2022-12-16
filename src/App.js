79dbe5e37fe2861a7579e6f0c5c660d0959d7d74
kathawkins <kmoreno1205@gmail.com>
2022-12-16 16:47:01 -0700

    added ChatLog (wave02)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
-import ChatEntry from './components/ChatEntry';
+// import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog.js';
 
 const App = () => {
-  const messageList = chatMessages[0];
-  // console.log(messageList);
-
   return (
     <div id="App">
       <header>
@@ -15,11 +13,7 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
-        <ChatEntry
-          sender={messageList.sender}
-          body={messageList.body}
-          timeStamp={messageList.timeStamp}
-        ></ChatEntry>
+        <ChatLog entries={chatMessages}></ChatLog>
       </main>
     </div>
   );
