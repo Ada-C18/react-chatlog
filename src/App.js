0df8c8c6821eef2b31e39c5239c62feb8e262f83
kathawkins <kmoreno1205@gmail.com>
2022-12-16 15:30:16 -0700

    ChatEntry displays one message (wave01)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const messageList = chatMessages[0];
+  // console.log(messageList);
+
   return (
     <div id="App">
       <header>
@@ -11,6 +15,11 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={messageList.sender}
+          body={messageList.body}
+          timeStamp={messageList.timeStamp}
+        ></ChatEntry>
       </main>
     </div>
   );
