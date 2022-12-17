7e45d9b65744511c7999be58f799768d87ec98d0
Anaiq <>
2022-12-17 12:14:20 -0600

    App component: import ChatEntry, render one ChatEntry. CharEntry component:import TimeStamp, use props to display data, add PropTypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
@@ -9,8 +10,13 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        {/* Wave 01: Render one ChatEntry component*/}
+        <ChatEntry 
+          sender="Joe Biden"
+          body="Get out by 8am.  I'll count the silverware"
+          timeStamp="2018-05-18T22:12:03Z"
+          />
+        {/*Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
