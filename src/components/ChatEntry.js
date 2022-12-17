87ad7cc3d63ab36b3bf95c0bb6f4bd3d4bb2152b
Younghee <kwak.younghee@gmail.com>
2022-12-17 12:37:17 -0800

    feat: ChatEntry works as desired, Wave 01 passed

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -7,16 +7,41 @@ const ChatEntry = (props) => {
   const body = props.body;
   const timeStamp = props.timeStamp;
 
+  const yearsAgo = yearconvert(timeStamp);
+  const timeStampStr = `${yearsAgo} years ago`;
+
   return (
     <div className="chat-entry local">
       <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
         <p>{body}</p>
-        <p className="entry-time">{timeStamp}</p>
+        <p className="entry-time">{timeStampStr}</p>
         <button className="like">🤍</button>
       </section>
     </div>
   );
+};
+
+const yearconvert = (timeStamp) => {
+  const today = new Date();
+  const ddToday = today.getDate();
+  const mmToday = today.getMonth() + 1;
+  const yyyyToday = today.getFullYear();
+  const yeardate = timeStamp.split('-');
+  const yyyy = parseInt(yeardate[0]);
+  const mm = parseInt(yeardate[1]);
+  const datetime = yeardate[2].split('T');
+  const dd = parseInt(datetime[0]);
+  let yearsAgo = yyyyToday - yyyy;
+  if (mmToday - mm < 0) {
+    // when the month is earlier than current month, it's not full years
+    yearsAgo = yearsAgo - 1;
+  } else if (mmToday - mm === 0) {
+    if (ddToday - dd < 0) {
+      yearsAgo = yearsAgo - 1;
+    }
+  }
+  return yearsAgo;
 };
 
 ChatEntry.propTypes = {
