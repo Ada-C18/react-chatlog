dce854173ec96cfb5d86cf6f40527a1cbe269a82
Aria Stroud <stroudaria@gmail.com>
2022-12-16 19:30:45 -0800

    Adds ChatEntry component to App. Uses props in ChatEntry to render one chat message. Passes wave 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const [messages, setMessages] = useState(chatMessages[0]);
+  // const something = messages.map((message) => {});
   return (
     <div id="App">
       <header>
@@ -11,6 +14,20 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <div>
+          <ChatEntry
+            sender={messages.sender}
+            body={messages.body}
+            timeStamp={messages.timeStamp}
+          />
+          {/* {messages.map((message) => (
+            <ChatEntry
+              sender={message.sender}
+              body={message.body}
+              timeStamp={message.timeStamp}
+            />
+          ))} */}
+        </div>
       </main>
     </div>
   );
