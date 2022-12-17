057c7bf907466698907349f68aeb1acf8289f697
Liat <liathirsh@gmail.com>
2022-12-16 22:00:14 -0500

    All chats displaying on webpage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
+import { useState } from 'react';
 
 const App = () => {
+  const [likesCount, updateLikesCount] = useState(0);
+  const increaseLikes = () => {
+    updateLikesCount(likesCount + 1);
+  };
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat Between Vladimir and Estragon </h1>
+        <h3> hearts display </h3>
       </header>
       <main>
+        {chatMessages.map((chat, i) => {
+          return (
+            <ChatEntry
+              key={chatMessages[i].id}
+              sender={chatMessages[i].sender}
+              body={chatMessages[i].body}
+              timeStamp={chatMessages[i].timeStamp}
+              liked={chatMessages[i].false}
+            />
+          );
+        })}
+        ;
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
