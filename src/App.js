05c8381e9ea52f25961e53b77ed38039bf642e74
Laura Skomerza <lskomerza@gmail.com>
2022-12-18 19:45:50 -0800

    finished wave 1 with updating chat entry func

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
+  console.log(chatMessages);
+  const chatEntry = <ChatEntry></ChatEntry>;
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
-      <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
-      </main>
+      <main>{chatEntry}</main>
     </div>
   );
 };
