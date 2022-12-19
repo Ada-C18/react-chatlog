3f8784cac1943ce774ffcecb5ba85bae3c7d9347
Annie <annie.gallagher7@gmail.com>
2022-12-18 22:09:55 -0700

    removed map to utilize data being passed in from ChatLog

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,48 +1,28 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
-const ChatEntry = ({ messages }) => {
-  // const individualMessage = props.messages.map((message) => {
-  //   return (
-  //     <div className="chat-entry local">
-  //       <h2 className="entry-name">{message.sender}</h2>
-  //       <section className="entry-bubble">
-  //         <p>{message.body}</p>
-  //         <p className="entry-time">{message.timeStamp}</p>
-  //         <button className="like">🤍</button>
-  //       </section>
-  //     </div>
-  //   );
-  // });
+const ChatEntry = (props) => {
   return (
     <div className="chat-entry local">
-      {messages.map((message) => {
-        return (
-          <div key={message.id}>
-            <h2 className="entry-name">{message.sender}</h2>
-            <section className="entry-bubble">
-              <p>{message.body}</p>
-              <p className="entry-time">{message.timeStamp}</p>
-              <button className="like">🤍</button>
-            </section>
-          </div>
-        );
-      })}
+      <h2 className="entry-name">{props.sender}</h2>
+      <section className="entry-bubble">
+        <p>{props.body}</p>
+        <p className="entry-time">
+          <TimeStamp time={props.timeStamp} />
+        </p>
+        <button className="like">🤍</button>
+      </section>
     </div>
   );
 };
 
 ChatEntry.propTypes = {
-  messages: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      sender: PropTypes.string.isRequired,
-      body: PropTypes.string.isRequired,
-      timeStamp: PropTypes.string.isRequired,
-      liked: PropTypes.bool.isRequired,
-    })
-  ),
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
 };
 
 export default ChatEntry;
