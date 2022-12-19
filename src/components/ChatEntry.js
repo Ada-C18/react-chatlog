ec849135764cedfa918a3afa2597affe09292ce6
Annie <annie.gallagher7@gmail.com>
2022-12-18 21:16:55 -0700

    maps messages and updates necessary JSX with prop from message

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -2,21 +2,47 @@ import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 
-const ChatEntry = (props) => {
+const ChatEntry = ({ messages }) => {
+  // const individualMessage = props.messages.map((message) => {
+  //   return (
+  //     <div className="chat-entry local">
+  //       <h2 className="entry-name">{message.sender}</h2>
+  //       <section className="entry-bubble">
+  //         <p>{message.body}</p>
+  //         <p className="entry-time">{message.timeStamp}</p>
+  //         <button className="like">🤍</button>
+  //       </section>
+  //     </div>
+  //   );
+  // });
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
-      <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
-        <button className="like">🤍</button>
-      </section>
+      {messages.map((message) => {
+        return (
+          <div key={message.id}>
+            <h2 className="entry-name">{message.sender}</h2>
+            <section className="entry-bubble">
+              <p>{message.body}</p>
+              <p className="entry-time">{message.timeStamp}</p>
+              <button className="like">🤍</button>
+            </section>
+          </div>
+        );
+      })}
     </div>
   );
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      sender: PropTypes.string.isRequired,
+      body: PropTypes.string.isRequired,
+      timeStamp: PropTypes.string.isRequired,
+      liked: PropTypes.bool.isRequired,
+    })
+  ),
 };
 
 export default ChatEntry;
