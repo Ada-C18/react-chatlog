90953dec456f6fc2a544c8104573778a8e70db7e
Annie <annie.gallagher7@gmail.com>
2022-12-18 22:08:18 -0700

    changed reference from ChatEntry to ChatLog

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import './App.css';
-import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog';
 import chatMessages from './data/messages.json';
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
-        <div>
-          <ChatEntry messages={chatMessages}></ChatEntry>
-        </div>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatLog messages={chatMessages}></ChatLog>
       </main>
     </div>
   );
