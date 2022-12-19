4c028d9ca62d5daa668bac79d783cebb60e93af4
Erika Sha <erikaasha05@gmail.com>
2022-12-19 01:03:28 -0500

    removed id from a chat entry

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -10,7 +10,6 @@ const ChatEntry = (props) => {
     <div className="chat-entry local">
       <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Id: {props.id}</p>
         <p>{props.body}</p>
         <p className="entry-time">
           <TimeStamp time={props.timeStamp} />
