4c1e81c7d3e9f2b638a1f2099e9f25f4c8068399
takhat <takhat014@gmail.com>
2022-12-19 16:21:40 -0800

    wave 1 done: Update the ChatEntry and App components to display a single chat message bubble with the message text and relative timestamp, plus the sender's name above it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.css';
-import chatMessages from './data/messages.json';
+// import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  //   const chatData = [
+  //     {
+  //       id: 1,
+  //       sender: 'Vladimir',
+  //       body: 'why are you arguing with me',
+  //       timeStamp: '2018-05-29T22:49:06+00:00',
+  //       liked: false,
+  //     },
+  //   ];
   return (
     <div id="App">
       <header>
@@ -11,6 +21,11 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender="Vladimir"
+          body="why are you arguing with me"
+          timeStamp="2018-05-29T22:49:06+00:00"
+        />
       </main>
     </div>
   );
--- src/components/TimeStamp.js
+++ src/components/TimeStamp.js
@@ -1,10 +1,10 @@
 import { DateTime } from 'luxon';
 
 const TimeStamp = (props) => {
-  const time = DateTime.fromISO(props.time);
+  const time = DateTime.fromISO(props.timeStamp);
+  console.log(time);
   const absolute = time.toFormat('MMMM Do YYYY, h:mm:ss a');
   const relative = time.toRelative();
-
   return <span title={absolute}>{relative}</span>;
 };
 
