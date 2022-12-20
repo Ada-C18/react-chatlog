0f626fd99e06e8b4e59c0d9a9201bf6254d4b02d
Aisha Husseini <ayasha.husseini@gmail.com>
2022-12-20 15:58:06 -0500

    working on wave 3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,77 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import ChatEntry from './components/ChatEntry';
 import ChatLog from './components/ChatLog';
 import chatMessages from './data/messages.json';
 
 const App = () => {
-  return (
+
+  const [entries, setEntries] = useState(chatMessages)
+
+  //This line of code is using the useState hook to define a state 
+  //variable called entries. The useState hook is a way to add state to
+  // functional components in React. The useState hook takes an initial value for the
+  // state variable as an argument, and returns an array with two elements: the current 
+  //value of the state variable, and a function for updating the state variable. In this case, 
+  //the initial value for entries is chatMessages.
+
+
+
+  const calcTotalLikes = () => {
+    return entries.reduce((total, entry) => {
+      return entry.liked ? total + 1 : total;
+    }, 0)
+    };
+
+    //This line of code is defining a function called calcTotalLikes.
+    //  When this function is called, it will iterate over the entries array 
+    //  and return the total number of items that have been liked. It does this by using
+    //   the reduce method, which takes a callback function as an argument. 
+    //   The callback function receives two arguments: total, which is the running total of 
+    //   the number of liked items, and entry, which is the current item being processed in the entries array. 
+    //   If the entry has a liked property that is true, then the running total is incremented by 1. If the entry has
+    //    a liked property that is false, then the running total is not incremented. The reduce method returns
+    //     the final running total after all items in the entries array have been processed.
+
+
+
+    const updateLike = (id) =>{
+    const newEntries = entries.map(entry => {
+      if (entry.id === id){
+        return { ...entry, liked: !entry.liked };
+      } else {
+        return entry
+      }
+    })
+    setEntries(newEntries)
+  }
+
+  // This line of code is defining a function called updateLike. 
+  // When this function is called with an id argument, it will update the 
+  // liked status of the item with the matching id in the entries array. 
+  // It does this by creating a new array called newEntries, which is a modified 
+  // version of the entries array. It does this by using the map method, which takes a 
+  // callback function as an argument. The callback function receives the current item 
+  // being processed in the entries array as an argument. If the current item has an id 
+  // that matches the id passed to the updateLike function, then the item is returned 
+  // with its liked property flipped using the not operator (!). If the current item does 
+  // not have a matching id, then it is returned as is. After the newEntries array has been created, 
+  // the setEntries function is called to update the entries state variable with the new array.
+
+
+  
+    return (
     <div id="App">
       <header>
         <h1>Chat Log</h1>
+        <h2> {calcTotalLikes()} ❤️s</h2>
       </header>
       <main>
-        <ChatLog entries={chatMessages} />
+        <ChatLog entries={chatMessages} updateLike = {updateLike} />
         {/* The "entries" here should match the entries in line 6 in ChatLog.
         <if we don't have props, it will only pass ChatMessages and won't pass the data from test/>
          */}
+
       </main>
     </div>
   );
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -4,6 +4,24 @@ import TimeStamp from './TimeStamp';
 import PropTypes from 'prop-types';
 
 const ChatEntry = (props) => {
+
+  const likeFill = props.liked ? '❤️' : '🤍';
+  //This line of code is using the ternary operator to set the value 
+  //of the likeFill constant. If props.liked is true, 
+  //then likeFill will be set to '❤️'. If props.liked is false, 
+  //then likeFill will be set to '🤍'. The likeFill constant will be 
+  //used later in the code to display either a red heart or a white
+  // heart emoji, depending on whether the item has been liked or not.
+  
+  const onLike = () => {
+    props.updateLike(props.id);
+  }
+  // This line of code is defining a function called onLike. When this function 
+  //is called, it will call the updateLike function, passing in the props.id as an argument.
+  // The updateLike function is likely a function that updates the like status of the item with the given id.
+  // The onLike function will be used later in the code as an event handler for a button or some other UI element 
+  //that allows the user to like or unlike an item.
+
   return (
     <div className="chat-entry local">
       <h2 className="entry-name">{props.sender}</h2>
@@ -12,7 +30,8 @@ const ChatEntry = (props) => {
         <p className="entry-time">
           <TimeStamp time={props.timeStamp} />
         </p>
-        <button className="like">🤍</button>
+        {/* <button className="like">🤍</button> */}
+        <button className="like" onClick={onLike}>{likeFill}</button>
       </section>
     </div>
   );
