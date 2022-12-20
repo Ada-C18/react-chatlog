e1f45b2bdfb7095be2824132faba5b47a8d1ae67
kimiahasani <kn.improve@gmail.com>
2022-12-19 22:06:14 -0800

    add propTypes and update likes

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,9 +1,19 @@
-import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
 
-const ChatEntry = ({ id, sender, body, timeStamp }) => {
+
+const ChatEntry = ({ id, sender, body, timeStamp, liked, updateLikes }) => {
+  const updateMessage = () => {
+    updateLikes({
+    
+      id,
+      sender,
+      body,
+      timeStamp,
+      liked: !liked,
+    });
+  };
   const comparePosition = id % 2 ? 'local' : 'remote';
   return (
     <div className={`chat-entry ${comparePosition}`}>
@@ -13,7 +23,9 @@ const ChatEntry = ({ id, sender, body, timeStamp }) => {
         <p className="entry-time">
           <TimeStamp time={timeStamp} />
         </p>
-        <button className="like">🤍</button>
+        <button className="like" onClick={updateMessage}>
+          {liked ? '❤️': '🤍' }
+        </button>
       </section>
     </div>
   );
@@ -25,6 +37,8 @@ ChatEntry.propTypes = {
   sender: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
   timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  updateLikes: PropTypes.func.isRequired,
 };
 
 export default ChatEntry;
