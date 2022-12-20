d7d49aaab70f01644625b0c32d0fe3eae7c8bc54
takhat <takhat014@gmail.com>
2022-12-19 22:43:50 -0800

    refactored initial data to be a deep copy of original data and made it a state variable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 // import ChatEntry from './components/ChatEntry';
 import ChatLog from './components/ChatLog';
 
 const App = () => {
+  const chatMessagesInitialCopy = chatMessages.map((chat) => {
+    return { ...chat };
+  });
+
+  const [chatsList, setChatsList] = useState(chatMessagesInitialCopy);
   return (
     <div id="App">
       <header>
@@ -19,7 +24,7 @@ const App = () => {
           timeStamp="2018-05-29T22:49:06+00:00"
         /> */}
         {/*Wave 02: Render ChatLog component */}
-        <ChatLog entries={chatMessages} />
+        <ChatLog entries={chatsList} />
       </main>
     </div>
   );
