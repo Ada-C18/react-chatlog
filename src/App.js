993b861013647c76116df611d5889122f236a708
Jennifer <datateur@gmail.com>
2022-12-20 11:42:42 -0800

    Completed wave 4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
+        {<ChatEntry 
         Wave 02: Render ChatLog component */}
       </main>
     </div>
