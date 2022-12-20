2f223f20ce1c894c306218c451bee530f09518d7
Yun <ly.yun811@gmail.com>
2022-12-19 16:47:23 -0800

    worked on Wave 1&2 static. Displying ChatEntry container. Need to add behavior

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between _ and _</h1>
+        <section>_❤s</section>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        {
+          <ChatEntry
+            chatEntryData={ChatEntry}
+          /> /* Wave 01: Render one ChatEntry component
+        Wave 02: Render ChatLog component */
+        }
       </main>
     </div>
   );
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -3,12 +3,56 @@ import './ChatEntry.css';
 import PropTypes from 'prop-types';
 
 const ChatEntry = (props) => {
+  const chatEntryId = props.id;
+  const chatEntrySender = props.sender;
+  const chatEntryBody = props.body;
+  const chatEntryTimeStamp = props.timeStamp;
+  const chatEntryLiked = props.liked;
+  // const updateLiked = props.updateLiked;
+  // const deleteChatEntry = props.deleteChatEntry;
+
+  // function updateLiked(inc) {
+  //   //inc is a boolean flag representing if we are increasing price or decreasing it
+  //   if (inc) {
+  //     //ChatEntryPrice += 1 WRONG
+  //     updatePrice(ChatEntryId, ChatEntryPrice + 1);
+  //   } else {
+  //     updatePrice(ChatEntryId, ChatEntryPrice - 1);
+  //   }
+  // }
+
+  // function getColorFromPrice(price) {
+  //   let myBudget = 100;
+  //   if (price <= myBudget) {
+  //     return "green";
+  //   } else {
+  //     return "red";
+  //   }
+  // }
+
+  // return (
+  // <div>
+  //   <h2 className="ChatEntry__name">{ChatEntryName}</h2>
+  //   <ul>
+  //     <li>{ChatEntrySender}</li>
+  //     <li>{ChatEntryBody}</li>
+  //     <li>{ChatEntryTimeStamp}</li>
+  //     <li><button
+  //     onClick={() => {
+  //       changeChatEntryPrice(true);
+  //     }}
+  //   >{ChatEntryLiked}</button></li>
+  //   </ul>
+  //   {/* <button onClick={() => deleteChatEntry(ChatEntryId)}>Delete</button> */}
+  // </div>
+  // );
+
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{chatEntrySender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{chatEntryBody}</p>
+        <p className="entry-time">{chatEntryTimeStamp}</p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -17,6 +61,11 @@ const ChatEntry = (props) => {
 
 ChatEntry.propTypes = {
   //Fill with correct proptypes
+  id: PropTypes.number.isRequired,
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
 };
 
 export default ChatEntry;
