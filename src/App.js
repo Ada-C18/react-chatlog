6e06b3ecf2d9bc4786a0f656f2304e1ed92b0685
Presley <persleytieman@outlook.com>
2022-12-20 18:03:27 -0500

    updated app

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
+  const messages = chatMessages.map((message) => (
+    <ChatEntry
+      sender={message.sender}
+      body={message.body}
+      timeStamp={message.timeStamp}
+      liked={message.liked}
+    ></ChatEntry>
+  ))
   return (
+
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* tried 
-        const messages = chatMessages.map(message => etc, can't seem to get it to work) */}
-        {chatMessages.map((message) => (
-          <ChatEntry
-            sender={message.sender}
-            body={message.body}
-            timeStamp={message.timeStamp}
-            liked={message.liked}
-          ></ChatEntry>
-        ))}
+        {messages}
       </main>
     </div>
   );
