a3d82f9227c7c7ad9c25e9ae5e94700b5821d987
hoku mcangus <mcangusohana2@gmail.com>
2022-12-20 14:14:32 -0800

    Wave 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import chatMessages from './data/messages.json';
+// import chatMessages from './data/messages.json';
 import ChatEntry from './components/ChatEntry';
 // import ChatLog from './components/ChatLog';
 
@@ -14,7 +14,13 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
-        <ChatEntry />
+        <ChatEntry 
+        
+          sender="Joe Biden"
+          body="Get out by 8am.  I'll count the silverware"
+          timeStamp="2018-05-18T22:12:03Z"
+      />
+        {/* console.log(ChatEntry); */}
         {/* <ChatLog entries={chatMessages}></ChatLog> */}
       </main>
     </div>
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
+// import messages.json from './data.messages.json';
 
 const ChatEntry = (props) => {
   return (
