09f4f0a0a8dfe9aa9ae4773cee1be3d196463ef1
dainiz <dainiz.almazan@gmail.com>
2022-12-20 13:56:28 -0800

    Connects App, ChatEntry, and TimeStamp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
@@ -9,8 +10,14 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry
+          id="1"
+          sender="Vladimir"
+          body="why are you arguing with me"
+          timeStamp="2018-05-29T22:49:06+00:00"
+          liked="false"
+        />
+        {/*Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
