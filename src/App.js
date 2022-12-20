786ca36d54354a88c470960646a8ddb606dab197
Maxwell <maxpasqualis@gmail.com>
2022-12-19 16:47:26 -0800

    added function to update liked messages' state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,19 @@ import chatMessages from './data/messages.json';
 import { useState } from 'react';
 
 const App = () => {
-  const [msgData, setMsgData] = useState([chatMessages]);
+  const [msgData, setMsgData] = useState(chatMessages);
+
+  const updateLiked = (likedMsg) => {
+    const messages = msgData.map((message) => {
+      if (message.id === likedMsg.id) {
+        return likedMsg;
+      } else {
+        return message;
+      }
+    });
+
+    setMsgData(messages);
+  };
 
   return (
     <div id="App">
@@ -16,7 +28,7 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
-        <ChatLog entries={msgData} />
+        <ChatLog entries={msgData} toggleLike={updateLiked} />
       </main>
     </div>
   );
