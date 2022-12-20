80dfbd92fb22fc68375a10fb0830a8c184b03374
Presley <persleytieman@outlook.com>
2022-12-20 17:45:48 -0500

    updated App, wave 1 finished w/ tests passing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
@@ -9,12 +10,24 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-          // entries={entries}
-        Wave 02: Render ChatLog component */}
+        {/* tried 
+        const messages = chatMessages.map(message => etc, can't seem to get it to work) */}
+        {chatMessages.map((message) => (
+          <ChatEntry
+            sender={message.sender}
+            body={message.body}
+            timeStamp={message.timeStamp}
+            liked={message.liked}
+          ></ChatEntry>
+        ))}
       </main>
     </div>
   );
 };
+
+
+
+
+
 
 export default App;
