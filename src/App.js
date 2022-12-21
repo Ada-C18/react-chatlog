e736220ba5e8d6d2a1a4f912a91e718d85e5a598
Sunny <1sunnymuniz@gmail.com>
2022-12-20 22:29:56 -0700

    modified app css file to account for section with heart like counts

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import ChatLog from './components/ChatLog';
 
 const App = () => {
   const [messages, setMessages] = useState(chatMessages);
+  const [likesCount, setLikesCount] = useState(0);
 
   const updateLikes = (id, like) => {
     const currentMessage = messages.find((message) => message.id === id);
     currentMessage.liked = like;
     setMessages([...messages]);
+    if (like) setLikesCount(likesCount + 1);
+    if (!like) setLikesCount(likesCount - 1);
   };
 
   return (
     <div id="App">
       <header>
         <h1>Chat between Vladimir and Estragon</h1>
+        <section>{likesCount} ❤️s</section>
       </header>
       <main>
         <ChatLog entries={chatMessages} setUpdateLikes={updateLikes} />
