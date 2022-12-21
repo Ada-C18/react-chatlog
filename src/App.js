98b40475f616d5adcc395ce3bdecbe0dea983bf4
Pavi <pavanirannulu@gmail.com>
2022-12-20 18:46:09 -0800

    completed wave 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const exampleMessage = chatMessages[0];
   return (
     <div id="App">
       <header>
@@ -11,6 +13,13 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry
+          id={exampleMessage['id']}
+          sender={exampleMessage['sender']}
+          body={exampleMessage['body']}
+          timeStamp={exampleMessage['timeStamp']}
+          liked={exampleMessage['liked']}
+        />
       </main>
     </div>
   );
