b551d8b133c0ed128d09bfece7e3bdc8d33167f3
Jessica <jessicaambrizmadrigal@gmail.com>
2022-12-20 18:09:41 -0800

    Modified some of wave 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
+        {console.log(chatMessages[0]['timeStamp'])}
       </header>
       <main>
+        <ChatEntry
+          sender="Jessica"
+          body="Hello World!"
+          timeStamp="2018-05-29T22: 49:06+00:00"
+        />
+        <ChatEntry
+          sender="Alma"
+          body="Hello World!"
+          timeStamp="2018-05-29T22: 49:06+00:00"
+        />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
