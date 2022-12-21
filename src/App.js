7c7f00dfe6d3d36e9455d5dae0b838004e6c2f5c
Alia Athman <aliaathmann@gmail.com>
2022-12-21 14:09:18 -0500

    Adds props to Chat Entry, passing wave 1 tests

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
+  const [messageData,setMessageData] = useState(chatMessages)
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
+        <ChatEntry 
+        sender='Alia'
+        body='Hello!'
+        timeStamp="2018-05-29T22:49:06+00:00"></ChatEntry>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
