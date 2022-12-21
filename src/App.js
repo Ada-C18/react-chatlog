a69c77947b5b321bf9270f68c73e1584ed70dca8
Tia Willis <tiawillis1@icloud.com>
2022-12-20 19:17:02 -0700

    wave 3 completed

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import ChatEntry from './components/ChatEntry';
 import ChatLog from './components/ChatLog';
 
 const App = () => {
+  const [likesCount, setLikesCount] = useState(0);
   const [messages, setMessages] = useState(chatMessages);
 
   const ToggleLike = (id, like) => {
     const foundMessage = messages.find((x) => x.id === id);
     foundMessage.liked = like;
     setMessages([...messages]);
+    if (like) setLikesCount(likesCount + 1);
+    if (!like) setLikesCount(likesCount - 1);
   };
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
+        <h2>{likesCount} ❤️s</h2>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
