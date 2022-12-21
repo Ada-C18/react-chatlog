921e0c7e614b89223ecbc523cc9a821838a04805
Luciane Dias <lucilewa@yahoo.com>
2022-12-20 22:57:22 -0800

    finished wave 1, 2 and 3

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import './ChatEntry.css';
+
+import TimeStamp from './TimeStamp';
 import PropTypes from 'prop-types';
 
 const ChatEntry = (props) => {
+  const changeHeart = () => {
+    props.toggleHeartCallback(props.id);
+  };
+
   return (
-    <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+    <div
+      className={`chat-entry ${
+        props.sender === 'Estragon' ? 'remote' : 'local'
+      }`}
+    >
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
-        <button className="like">🤍</button>
+        <p>{props.body}</p>
+        <p className="entry-time">
+          <TimeStamp time={props.timeStamp} />
+        </p>
+        <button className="like" onClick={changeHeart}>
+          {props.liked ? '❤️' : '🤍'}
+        </button>
       </section>
     </div>
   );
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  id: PropTypes.number,
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool,
+  toggleHeartCallback: PropTypes.func,
+};
+
+ChatEntry.defaultProps = {
+  toggleHeartCallback: () => {},
 };
 
 export default ChatEntry;
