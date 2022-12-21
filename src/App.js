455f619b977c28a3127d3b215a7f7f9f17634fd6
Lourdes Zaragoza <lourdes.zaragoza34@gmail.com>
2022-12-20 18:16:05 -0800

    like counter working as well as liked toggle but wave 3 is still failing

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const App = () => {
     <div id="App">
       <header>
         <h1>Chat between Vladimir and Estragon</h1>
-        <h2>Like Counter: {totalLikes} </h2>
+        <h2>{totalLikes} ❤️s</h2>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
