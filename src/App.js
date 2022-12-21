8db54c07830399dcfe8bf20fea3bb482efab7159
Silvia <sx0771@gmail.com>
2022-12-20 21:50:30 -0500

    wave1+wave1/2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
+
 
 const App = () => {
+  const messages = chatMessages.map((message) => {
+    return {message};
+  });
+
+  const [chatData, setChatData] = useState(messages);
+
   return (
-    <div id="App">
+    <div id="App">  
       <header>
         <h1>Application title</h1>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry />
       </main>
     </div>
   );
