16971de1a128f4767066f95e87e2fc4544412374
Tia Willis <tiawillis1@icloud.com>
2022-12-21 20:54:18 -0700

    test issue resolved, updated code with eventlistener & DRY

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
-import ChatEntry from './components/ChatEntry';
 import ChatLog from './components/ChatLog';
 
 const App = () => {
   const [likesCount, setLikesCount] = useState(0);
-  const [messages, setMessages] = useState(chatMessages);
+  document.addEventListener('onLikeToggle', (likedEvt) => {
+    likedEvt.stopPropagation();
+    likedEvt.detail.liked
+      ? setLikesCount(likesCount + 1)
+      : setLikesCount(likesCount - 1);
+  });
 
-  const ToggleLike = (id, like) => {
-    const foundMessage = messages.find((x) => x.id === id);
-    foundMessage.liked = like;
-    setMessages([...messages]);
-    if (like) setLikesCount(likesCount + 1);
-    if (!like) setLikesCount(likesCount - 1);
-  };
   return (
     <div id="App">
       <header>
@@ -25,7 +22,7 @@ const App = () => {
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
         <div>
-          <ChatLog setToggleLike={ToggleLike} entries={chatMessages} />
+          <ChatLog entries={chatMessages} />
         </div>
       </main>
     </div>
