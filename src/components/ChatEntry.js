cf2a2e35523710169c6fa3633ab6c089fa3fbb2a
nina-tuyen tran <ninatuyentran@gmail.com>
2022-12-22 12:31:29 +0700

    Rendered TimeStamp in ChatEntry.js to show the time since message was posted to pass a test in wave 01

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
   const id = props.id
   const sender = props.sender;
   const body = props.body;
-  const timeStamp = props.timeStamp;
-  const liked = props.liked
-  const updateChatLog = props.updateChatLog
-
+  const liked = props.liked;
+  const updateChatLog = props.updateChatLog;
+  const timeStamp = props.timeStamp
 
   const determineSender = (sender) => {
     if (sender === 'Vladimir'){
@@ -35,8 +35,9 @@ const ChatEntry = (props) => {
       <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
         <p>{body}</p>
-        <p className="entry-time">{timeStamp}</p>
+        <p className="entry-time"> {timeStamp} </p>
         <button className="like"  onClick={() => updateHeartColor(liked)}>{displayHeart(liked)}</button>
+        <TimeStamp time={props.timeStamp}/>
       </section>
     </div>
   );
@@ -45,11 +46,9 @@ const ChatEntry = (props) => {
 
 
 ChatEntry.propTypes = {
-  id: PropTypes.number.isRequired,
   sender: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
   timeStamp: PropTypes.string.isRequired,
-  updateChatLog:PropTypes.func.isRequired
 };
 
 export default ChatEntry;
