aeb957b69e3108ad01477607fefa756a15e14532
Maria Silva <hello@mariasilva.dev>
2022-12-22 00:40:11 -0600

    completed wave 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatLog from './components/ChatLog';
+
+let totalLikes = 0;
+
+  const updateLikes = (chatData) => {
+    let likes = 0;
+    for (const chat of chatData) {
+      if (chat.liked) {
+        likes += 1;
+      } 
+    }
+    totalLikes = likes;
+  };
 
 const App = () => {
+  const [chatData, setChatData] = useState(chatMessages);
+  const updateChatData = (updatedChat) => {
+    const entries = chatData.map((newChat) => {
+      if (newChat.id === updatedChat.id) {
+        return updatedChat;
+      } else {
+        return newChat;
+      }
+    });
+    setChatData(entries);
+    updateLikes(entries);
+  };
+  
+
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
+        <section className="header section">
+        <h2 id="heartWidget" className="widget">{totalLikes} ❤️s</h2>
+        </section>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatLog entries={chatData} onUpdateChat={updateChatData}></ChatLog>
       </main>
     </div>
   );
