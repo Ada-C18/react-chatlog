31165f40aec711bf435940446cbc6f91093ffc2e
Katherine Abrikian <kabrikian@gmail.com>
2022-12-21 23:39:55 -0500

    wave 1 passed

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import './App.css';
 import chatMessages from './data/messages.json';
 import ChatEntry from './components/ChatEntry';
 
-// const kMessageTest = {
+// {
 //   id: 1,
 //   sender: 'Vladimir',
 //   body: 'why are you arguing with me',
 //   timeStamp: '2018-05-29T22:49:06+00:00',
 //   liked: false,
-// };
+// }
 
-// {id, sender, body, timeStamp, liked}
 const kMessage1 = chatMessages[0];
-
-// I will need to format the timestamp. I'll come back to that.
 
 const App = () => {
   return (
