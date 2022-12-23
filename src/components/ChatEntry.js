9b3b1feda1e177914cc83c078ecc432bc231134b
Tami <tamiadetayo@gmail.com>
2022-12-23 23:01:40 +0800

    Finished wave 1

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
-const ChatEntry = (props) => {
-  return (
-    <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
-      <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
-        <button className="like">🤍</button>
-      </section>
-    </div>
-  );
+const ChatEntry = ({sender, body, timeStamp}) => {
+    return (
+      <div className="chat-entry local">
+        <h2 className="entry-name">{sender}</h2>
+        <section className="entry-bubble">
+          <p>{body}</p>
+          <p className="entry-time"><TimeStamp time={timeStamp}></TimeStamp></p>
+          <button className="like">🤍</button>
+        </section>
+      </div>
+    );
 };
 
 ChatEntry.propTypes = {
