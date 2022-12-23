82a44a2b97393151c2f37edc3525e1d4230b2639
Jacqueline Hopkins <318jlh@gmail.com>
2022-12-23 13:56:26 -0800

    update the ChatEntry and App components

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
+  // const chatData = [
+  //   {
+  //     id: 1,
+  //     sender: 'Vladimir',
+  //     body: 'why are you arguing with me',
+  //     timeStamp: '2018-05-29T22:49:06+00:00',
+  //     liked: false,
+  //   },
+  // ];
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{props.body}</p>
+        <p className="entry-time">{props.TimeStamp}</p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -16,7 +26,9 @@ const ChatEntry = (props) => {
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
 };
 
 export default ChatEntry;
