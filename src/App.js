f5555a8487040fcdee8708e78462b5e4acb076b3
Anika Stephen Wilbur <anika.m.stephen@gmail.com>
2023-01-18 23:15:14 -0800

    Start building out App and ChatEntry components.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ChatEntry from './components/ChatEntry';
 import './App.css';
 import chatMessages from './data/messages.json';
 
 const App = () => {
+  // const [, setBoardsData] = useState([]);
+
+  // const messageLog = () => {
+  //   return (
+  //     chatMessages
+  //   )
+  // }
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
+        <div className="messageContainer">
+          <ChatEntry
+            sender={chatMessages.sender}
+            body={chatMessages.body}
+            timeStamp={chatMessages.timeStamp}
+            // {messageLog}
+          ></ChatEntry>
+        </div>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
