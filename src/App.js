dfc996cbe2b4344a9a302f789ed7681a6b782d89
maple-megan333 <maple.megan333@gmail.com>
2023-01-26 22:21:28 -0800

    passing all but last one

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import ChatLog from './components/ChatLog.js';
 
 const App = () => {
+  const likeCount = chatMessages.reduce((a, obj) => {
+    return a + obj.liked;
+  }, 0);
+
+  const updateLikes = () => {
+    return 0;
+  };
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
+        <h2>{likeCount}'❤️'s</h2>
       </header>
-      <main>{<ChatLog entries={chatMessages}></ChatLog>}</main>
+      <main>
+        {
+          <ChatLog
+            entries={chatMessages}
+            likeCount={likeCount}
+            updateLikes={updateLikes}
+          ></ChatLog>
+        }
+      </main>
     </div>
   );
 };
